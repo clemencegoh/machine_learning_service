cf6174972c53a3539a10ef19ec7100f593506826
Clemence Goh <clemencegoh@users.noreply.github.com>
2020-11-02 11:24:27 +0800

    add case study problem

--- food_delivery/models.py
+++ food_delivery/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    order_fields = models.JSONField()
+
+
+# todo: complete this validation function for below JSON field
+def validate_order(self, value):
+    pass
+
+
+class Order(models.Model):
+    # create foreign key so that each order can be referenced to a Restaurant in a many-to-one rls
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    # flexible order, requires custom validations depending on restaurant
+    order_details = models.JSONField()
+
